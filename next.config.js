ec211937d895f59547ad1caf8f852be9c5e62f07
Chris Keller <xylo04@gmail.com>
2023-05-14 17:23:17 -0600

    Redirect homepage to 217 list
    That's the only useful page right now.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/217a',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
