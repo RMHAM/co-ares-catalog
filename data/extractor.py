35378fa1a556ecd0f1a4e6025dae40e1030bf166
Chris Keller <xylo04@gmail.com>
2023-05-14 16:27:03 -0600

    Upload changes to mongodb

--- data/extractor.py
+++ data/extractor.py
@@ -1,11 +1,14 @@
 """
-Extract Form 217a data from an Excel file and export it to a CSV file.
+Extract Form 217a data from an Excel file and upload to MongoDB.
 
 Usage: python extractor.py <filename.xlsx>
 """
 
 import sys
+import os
+import re
 import pandas as pd
+from pymongo import MongoClient
 
 
 def parse_excel_217(filename):
@@ -15,36 +18,42 @@ def parse_excel_217(filename):
     # read the data from the first sheet in the Excel file
     df = pd.read_excel(filename, sheet_name=0, header=2, dtype='string')
     df.rename(columns={
-        "Channel Name/Trunked Radio System Talkgroup": "Channel Name",
-        "RX Freq N or W": "RX Freq",
-        "Unnamed: 5": "RX Width",
-        "TX Freq N or W": "TX Freq",
-        "Unnamed: 8": "TX Width",
-        "Mode A, D or M": "Mode",
+        "Pg No": "page",
+        "Channel Configuration": "config",
+        "Channel Name/Trunked Radio System Talkgroup": "name",
+        "Eligible Users": "eligibleUsers",
+        "RX Freq N or W": "rxFreq",
+        "Unnamed: 5": "rxWidth",
+        "RX Tone/NAC": "rxTone",
+        "TX Freq N or W": "txFreq",
+        "Unnamed: 8": "txWidth",
+        "Tx Tone/NAC": "txTone",
+        "Mode A, D or M": "mode",
+        "Remarks": "remarks",
     }, inplace=True)
 
     # filter the rows
-    df = df[df['Pg No'].str.isnumeric()]
-    df = df[df['Channel Name'].notna()]
+    df = df[df['page'].str.isnumeric()]
+    df = df[df['name'].notna()]
 
     # clean up data
-    df['Remarks'] = df['Remarks'].str.replace('\n', ' ')
-    df['RX Freq'] = df['RX Freq'].str.removesuffix('LSB')
-    df['TX Freq'] = df['TX Freq'].str.removesuffix('LSB')
-    df['RX Freq'] = df['RX Freq'].str.removesuffix('USB')
-    df['TX Freq'] = df['TX Freq'].str.removesuffix('USB')
-    df['RX Freq'] = df['RX Freq'].str.replace('****', '0')
-    df['TX Freq'] = df['TX Freq'].str.replace('****', '0')
+    df['remarks'] = df['remarks'].str.replace('\n', ' ')
+    df['rxFreq'] = df['rxFreq'].str.removesuffix('LSB')
+    df['txFreq'] = df['txFreq'].str.removesuffix('LSB')
+    df['rxFreq'] = df['rxFreq'].str.removesuffix('USB')
+    df['txFreq'] = df['txFreq'].str.removesuffix('USB')
+    df['rxFreq'] = df['rxFreq'].str.replace('****', '0')
+    df['txFreq'] = df['txFreq'].str.replace('****', '0')
 
     # change data types for the freq columns
     df = df.astype({
-        'RX Freq': 'float',
-        'TX Freq': 'float',
+        'rxFreq': 'float',
+        'txFreq': 'float',
     })
 
     # round the freq columns to 5 decimal places
-    df['RX Freq'] = df['RX Freq'].round(5)
-    df['TX Freq'] = df['TX Freq'].round(5)
+    df['rxFreq'] = df['rxFreq'].round(5)
+    df['txFreq'] = df['txFreq'].round(5)
     return df
 
 
@@ -56,6 +65,37 @@ filename = sys.argv[1]
 print(filename)
 df = parse_excel_217(filename)
 
-# export the data to a CSV file
-out_filename = filename.removesuffix('.xlsx') + '.csv'
-df.to_csv(out_filename, index=False)
+# Group by the first 2-3 numbers and one character of the name column
+df['group'] = df['name'].str.extract(r'^(\d{2,3}\w)')
+
+# for each group, insert a document into the database
+connection_string = os.environ.get('MONGODB_CONNECTION_STRING')
+client = MongoClient(connection_string, 27017)
+db = client['test']
+collection = db['form217as']
+for group in df['group'].unique():
+    df_group = df[df['group'] == group]
+    df_group.drop(columns=['group'], inplace=True)
+    df_group['order'] = df_group.index + 1
+
+    matches = re.search(r'^(\d{1,2})(\d)(\w)', group)
+    region = matches.group(1)
+    district = matches.group(2)
+    band = matches.group(3)
+    channels = df_group.to_dict('records')
+
+    # Map band character to band name
+    band = {
+        'D': 'Digital',
+        'H': 'HF',
+        'P': 'Packet',
+        'U': 'UHF',
+        'V': 'VHF',
+    }[band]
+
+    owner = f'Region {region} District {district}'
+    if region == '1' and district == '0':
+        owner = 'Colorado Section'
+    form217doc = dict({'owner': owner, 'frequencyBand': band,
+                      'channels': channels})
+    collection.insert_one(form217doc)
