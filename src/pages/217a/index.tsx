8b48d0583d6d26aa7e45a52d69a2aa97d899617e
Chris Keller <xylo04@gmail.com>
2023-05-25 14:35:42 -0600

    Group channels by district

--- src/pages/217a/index.tsx
+++ src/pages/217a/index.tsx
@@ -4,23 +4,32 @@ import dbConnect from "@/lib/dbConnect";
 import { Form217A, IForm217A } from "@/models/form217a.model";
 
 type List217AProps = {
-  f217s: IForm217A[];
+  grouped217s: any;
 };
 
-export default function List217A({ f217s }: List217AProps) {
+export default function List217A({ grouped217s }: List217AProps) {
   return (
     <>
       <h2>ICS-217A Catalog</h2>
-      <ul>
-        {f217s &&
-          f217s.map((f217) => (
-            <li key={f217._id.toString()}>
-              <Link href={"/217a/" + f217._id}>
-                {f217.description} - {f217.frequencyBand}
-              </Link>
-            </li>
-          ))}
-      </ul>
+      {grouped217s &&
+        Object.keys(grouped217s).map((ownerId) => {
+          const f217s: IForm217A[] = grouped217s[ownerId]!;
+          return (
+            <div key={ownerId}>
+              <div>{f217s[0].description}</div>
+              <ul>
+                {f217s &&
+                  f217s.map((f217) => (
+                    <li key={f217._id.toString()}>
+                      <Link href={"/217a/" + f217._id}>
+                        {f217.frequencyBand}
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          );
+        })}
     </>
   );
 }
@@ -30,10 +39,19 @@ export async function getServerSideProps() {
   const f217s = await Form217A.find().sort({
     description: "asc",
     frequencyBand: "asc",
-  });  
+  });
+  const flat217s = f217s.map((f) => JSON.parse(JSON.stringify(f)));
+  // group by ownerId
+  const grouped217s = flat217s.reduce<Map<string, IForm217A[]>>((acc, f217) => {
+    const ownerId: string = f217.ownerId;
+    if (!acc.has(ownerId)) {
+      acc.set(ownerId, []);
+    }
+    acc.get(ownerId)!.push(f217);
+    return acc;
+  }, new Map<string, IForm217A[]>());
 
-  console.log(f217s);
   return {
-    props: { f217s: f217s.map((f) => JSON.parse(JSON.stringify(f))) },
+    props: { grouped217s: Object.fromEntries(grouped217s) },
   };
 }
