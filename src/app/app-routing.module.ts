5fe97ad5385fcf8e2999b007328a9ecd1a4b52de
Chris Keller <xylo04@gmail.com>
2023-06-16 12:30:43 -0600

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Form217DetailComponent } from './form217-detail/form217-detail.component';
+import { Form217ListComponent } from './form217-list/form217-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'f217', component: Form217ListComponent },
+  { path: 'f217/:id', component: Form217DetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
