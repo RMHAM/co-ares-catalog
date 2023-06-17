61fd3b51923b40a6af5c250aa278f2f905fdf14b
Chris Keller <xylo04@gmail.com>
2023-06-17 16:23:03 -0600

    Various small fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
@@ -5,12 +6,12 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatMenuModule } from '@angular/material/menu';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,28 +27,29 @@ import { OrganizationTitlePipe } from './organization-title.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    Ics217DetailComponent,
-    MainNavComponent,
-    OrgTreeComponent,
-    OrgBranchComponent,
-    OrderByPipe,
-    OrganizationTitlePipe,
-    LoginComponent,
     AvatarComponent,
+    Ics217DetailComponent,
+    LoginComponent,
+    MainNavComponent,
+    OrderByPipe,
+    OrgBranchComponent,
+    OrgTreeComponent,
+    OrganizationTitlePipe,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    BrowserModule,
     MatButtonModule,
-    MatSidenavModule,
     MatIconModule,
     MatListModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
     MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    NgOptimizedImage,
+    provideAuth(() => getAuth()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
