c036828e01b1706caaa7b5b6045ee3fd53897ba7
Chris Keller <xylo04@gmail.com>
2023-06-19 11:50:34 -0600

    Add Firebase Admin with query

--- data/src/main.ts
+++ data/src/main.ts
@@ -1,34 +1,20 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import { applicationDefault, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+const app = initializeApp({
+  credential: applicationDefault(),
+});
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
+const db = getFirestore(app);
+const query = db
+  .collection('organizations')
+  .where('region', '==', 1)
+  .where('district', '==', 6)
+  .limit(1);
+const result = await query.get();
+if (result.empty) {
+  console.log('No such document!');
+} else {
+  const org = result.docs[0].data();
+  console.log('Found org:', org.name);
 }
