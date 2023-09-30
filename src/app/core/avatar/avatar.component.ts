9a6f9d0d9a5ca71a141de95f5a4c3f80b4af0857
Chris Keller <xylo04@gmail.com>
2023-09-30 15:37:59 -0600

    Change Avatar to using Signals

--- src/app/core/avatar/avatar.component.ts
+++ src/app/core/avatar/avatar.component.ts
@@ -1,36 +1,25 @@
-import { Component, inject, OnDestroy } from '@angular/core';
-import { Auth, User, user } from '@angular/fire/auth';
-import { BehaviorSubject, Subscription } from 'rxjs';
+import { Component, computed, inject } from '@angular/core';
+import { Auth, user } from '@angular/fire/auth';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
 })
-export class AvatarComponent implements OnDestroy {
+export class AvatarComponent {
   readonly blankPhotoUrl: string =
     'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png';
   private auth: Auth = inject(Auth);
 
-  private user$ = user(this.auth);
-  private userSubscription: Subscription;
-  loggedIn$ = new BehaviorSubject<boolean>(false);
-  photoURL$ = new BehaviorSubject<URL>(new URL(this.blankPhotoUrl));
-  displayName$ = new BehaviorSubject<string>('Anonymous');
-  email$ = new BehaviorSubject<string>('me@example.com');
+  private user = toSignal(user(this.auth));
+  loggedIn = computed(() => !!this.user());
+  photoURL = computed(
+    () => new URL(this.user()?.photoURL ?? this.blankPhotoUrl)
+  );
+  displayName = computed(() => this.user()?.displayName ?? 'Anonymous');
+  email = computed(() => this.user()?.email ?? 'me@example.com');
 
-  constructor() {
-    this.userSubscription = this.user$.subscribe((user: User | null) => {
-      this.loggedIn$.next(!!user);
-      this.photoURL$.next(new URL(user?.photoURL ?? this.blankPhotoUrl));
-      this.displayName$.next(user?.displayName ?? 'Anonymous');
-      this.email$.next(user?.email ?? 'me@example.com');
-    });
-  }
-
-  ngOnDestroy() {
-    this.userSubscription.unsubscribe();
-  }
   async logout() {
     await this.auth.signOut();
   }
