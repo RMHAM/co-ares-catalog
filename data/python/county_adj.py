4abd0161adc372569344edd84e9c1511d7de3ac2
Chris Keller <xylo04@gmail.com>
2023-10-13 22:25:45 -0600

    Export as JSON

--- data/python/county_adj.py
+++ data/python/county_adj.py
@@ -20,14 +20,14 @@ placemarks = list(folder[0].features())
 print("read in", len(placemarks), "counties")
 
 # Dictionary of GEO ID to county name
-geoid_to_name = {}
+geoid_to_dict = {}
 # Dictionary of point to set of GEO IDs
 point_to_geoids = {}
 
 for placemark in placemarks:
     geo_id = None
     county_name = None
-    state_name  = None
+    state_name = None
     ex_data = list(placemark.extended_data.elements)[0]
     for dat in ex_data._data:
         if dat['name'] == 'NAMELSAD':
@@ -39,9 +39,9 @@ for placemark in placemarks:
         if dat['name'] == 'STATE_NAME':
             state_name = dat['value'].strip()
             continue
-    geoid_to_name[geo_id] = county_name + ', ' + state_name
-    print(geo_id, geoid_to_name[geo_id])
- 
+    geoid_to_dict[geo_id] = {'name': county_name + ', ' + state_name}
+    print(geo_id, geoid_to_dict[geo_id])
+
     if type(placemark.geometry) == pygeoif.geometry.Polygon:
         geoms = [placemark.geometry]
     else:
@@ -55,26 +55,23 @@ for placemark in placemarks:
 
 print(len(point_to_geoids), "points")
 
-# Dictionary of GEO ID to set of GEO IDs
-adj = {}
 for point in point_to_geoids:
-    geoids = point_to_geoids[point]
-    for geoid1 in geoids:
-        if geoid1 not in adj:
-            adj[geoid1] = set()
-        for geoid2 in geoids:
+    counties_at_point = point_to_geoids[point]
+    for geoid1 in counties_at_point:
+        if 'adj' not in geoid_to_dict[geoid1]:
+            geoid_to_dict[geoid1]['adj'] = set()
+        for geoid2 in counties_at_point:
             if geoid1 != geoid2:
-                adj[geoid1].add(geoid2)
+                geoid_to_dict[geoid1]['adj'].add(geoid2)
 
-print("writing out adjacency matrix")
-with open('county_adj.txt', 'w') as out_file:
-    for geoid1 in adj:
-        for geoid2 in adj[geoid1]:
-            out_file.write(geoid1 + '\t' + geoid2 + '\n')
+# write out adjacency matrix as json
+import json
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
 
-print("writing out GEO ID to county name")
-with open('geoid_to_name.txt', 'w') as out_file:
-    for geoid in geoid_to_name:
-        out_file.write(geoid + '\t' + geoid_to_name[geoid] + '\n')
-
+with open('county_adj.json', 'w') as outfile:
+    json.dump(geoid_to_dict, outfile, default=set_default)
+    
 print("done")
