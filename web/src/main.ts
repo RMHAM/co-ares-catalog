3e5966c13c5b0709d5868ed1ef9dcbdaef49ff05
Chris Keller <xylo04@gmail.com>
2024-05-29 13:49:27 -0600

    Upgrade to Angular 18

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,7 +3,6 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app/app-routing.module';
@@ -12,13 +11,10 @@ import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(
-      AppRoutingModule,
-      BrowserModule,
-      provideAuth(() => getAuth()),
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-    ),
+    importProvidersFrom(AppRoutingModule, BrowserModule),
     provideAnimations(),
+    provideAuth(() => getAuth()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
 }).catch((err) => console.error(err));
