fcc8819e406ceefb927b5f1da1173ec1f65c0135
Chris Keller <xylo04@gmail.com>
2024-06-10 18:07:03 -0600

    Revert "Upgrade to Angular 18"
    This reverts commit d5fb6fac211a5433a8b705caa78e70c021690ea3.

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,6 +3,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app/app-routing.module';
@@ -11,10 +12,13 @@ import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(AppRoutingModule, BrowserModule),
+    importProvidersFrom(
+      AppRoutingModule,
+      BrowserModule,
+      provideAuth(() => getAuth()),
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+    ),
     provideAnimations(),
-    provideAuth(() => getAuth()),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
   ],
 }).catch((err) => console.error(err));
